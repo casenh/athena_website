4b29d72c3b7bb8f900a59d37be2b5a3d5e33345f
Casen Hunger <casen@Casens-MacBook-Air.local>
2015-07-25 13:46:22 -0500

    Updating the /data route

--- athena_website/controllers/run.py
+++ athena_website/controllers/run.py
@@ -7,3 +7,15 @@ from flask import render_template
 @app.route("/", methods=["GET"])
 def index():
     return render_template('index.html')
+
+# Rending the data page
+@app.route("/data", methods=["GET"])
+def get_data():
+    print("Routing data")
+    return render_template('data.html')
+
+# Collect post information
+@app.route("/data", methods=["POST"])
+def post_data():
+    print("GOT DATA")
+    print(request.json)
